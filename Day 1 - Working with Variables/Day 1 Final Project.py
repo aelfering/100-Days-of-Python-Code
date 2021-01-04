679513af32b6f7c71f6693e2bbfb940c49b6aa56
Alex Elfering <anelfering@outlook.com>
2021-01-04 10:49:01 -0600

    code added in

--- Day 1 - Working with Variables/Day 1 Final Project.py
+++ Day 1 - Working with Variables/Day 1 Final Project.py
@@ -1,11 +1,16 @@
 # Day 1 Exercise
 
 #1. Create a greeting for the program
+print("Welcome!")
 
 #2. Ask the user for the city that they grew up in
+city = input("What is the name of the city that you grew up in?")
 
 #3. Ask the user for the name of a pet
+pet = input("What is the name of your pet, or a friend's pet?")
 
 #4. Combine the name of their city and pet and show them the name of their band
+print("Your band should be called " + city + " " + pet)
+
 
 #5. Make sure the input cursor shows on a new line
