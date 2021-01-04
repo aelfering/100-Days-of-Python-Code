7ed5566457040d6fd9f701f0f7cf475c70471f05
Alex Elfering <anelfering@outlook.com>
2021-01-04 10:46:53 -0600

    updated the notes and day 1 project

---
+++ Day 1 - Working with Variables/Day 1 Final Project.py
@@ -0,0 +1,11 @@
+# Day 1 Exercise
+
+#1. Create a greeting for the program
+
+#2. Ask the user for the city that they grew up in
+
+#3. Ask the user for the name of a pet
+
+#4. Combine the name of their city and pet and show them the name of their band
+
+#5. Make sure the input cursor shows on a new line
--- Day 1 - Working with Variables/Day-1-Printing-Start.py
+++ Day 1 - Working with Variables/Day-1-Printing-Start.py
@@ -41,10 +41,10 @@ print("Hello" + " " + "Alex Elfering")
 
 ####    CODING CHALLENGE 2  ####
 
-#   How to debug each line
+#   How to debug code by each line
 
 print(Day - 1 String Manipulation")
-#   quotation marks are missing at the beginning of the string
+#   quotation marks are missing at the beginning of the string. Python thinks everything before the quote is code
 #   solution:
 print("Day 1 - String Manipulation")
 
@@ -62,6 +62,133 @@ print(("New lines can be created with a backslash and n.")
 #   too many parentheses on the left-hand side
 #   solution:
 print("New lines can be created with a backslash and n.")
+
+
+
+
+
+
+
+
+
+
+####    Python Input Statement  ####
+
+print("Hello" + " " + "Alex Elfering")
+
+#   what if we want to input data?
+
+input("a prompt for the user")
+input("What is your name?")
+#   the script will then prompt you with this question followed by a cursor ready for data entry. If you answer it with a string, then it is returned in the prompt
+
+print("Hello, " + input("What is your name?"))
+#   here we have a nested statement
+#   The script will prompt you for your name. If you enter a string, then the script will respond "Hello ___ "
+
+#   thonny.org is a good resource to utilize
+
+
+
+####    coding challenge 3  ####
+
+#   idea: if a user types in their name, the script should return the length of their name
+
+#   len() is the length of the string
+
+print(len(input("What is your name?")))
+
+#   first you create your input statement
+#   then you use the len() function to wrap the input 
+#   lastly print the results
+
+
+
+
+
+
+
+
+
+
+
+####    PYTHON VARIABLES    ####
+
+input("What is your name?")
+#   there is no way (yet) to refer to the input to this prompt
+
+
+#   variables!
+name = input("What is your name?")
+#   the answer to this input is stored in the variable name
+
+nameperson = 'Alex Elfering'
+print(nameperson)
+
+nameperson = 'Zach Simms'
+print(nameperson)
+
+#   make sure to not overwrite variable names if you don't want that
+
+namequestion = input('What is your name?')
+print(len(namequestion))
+
+####    CODING CHALLENGE 4  ####
+a = input("a: ")
+b = input("b: ")
+
+#   how do you switch the contents of a and b without rewriting the code?
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
+
+
+#   what are the rules of naming variables?
+#       make the code readable!
+#       numbers are only allowed after a character 
+#       good practice not to use function names as variable names
+#       make sure to spell variable names correctly 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
