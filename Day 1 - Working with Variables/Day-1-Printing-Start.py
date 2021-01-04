362d705526a815904ce13e5d56fda144ad7eca84
Alex Elfering <anelfering@outlook.com>
2021-01-04 09:46:32 -0600

    added debugging exercise

--- Day 1 - Working with Variables/Day-1-Printing-Start.py
+++ Day 1 - Working with Variables/Day-1-Printing-Start.py
@@ -2,3 +2,116 @@
 #   the quotation marks identify a string
 
 print("hello world!")
+
+#   don't forget to close out the quotation marks or parentheses, otherwise a syntax error
+#   you can always copy and paste the error message into Google to usually pull up StackOverFlow
+
+#   the color of your code will help guide your script if something is wrong
+
+
+
+####   CODING CHALLENGE #1  ####
+print('what to print')
+
+#   this should print the string "what to print"
+
+print("print('what to print')")
+
+#   if you are using quotes within quotes then use double quotes on the outside AND single quotes on the bottom 
+
+
+
+
+
+
+
+
+####    STRING MANIPULATION ####
+#   to write on a new line within a string when printing, use \n
+
+print("Hello world\nHello world\nHello world\nHello world")
+
+#   this saves lines
+
+#   we can also concatenate strings together 
+
+print("Hello" + " " + "Alex Elfering")
+
+#   spaces matter when using python! Mind indentation errors
+
+####    CODING CHALLENGE 2  ####
+
+#   How to debug each line
+
+print(Day - 1 String Manipulation")
+#   quotation marks are missing at the beginning of the string
+#   solution:
+print("Day 1 - String Manipulation")
+
+print("String concatenation is done with the "+" sign.")
+#   this is correct, but I think they want to include the plus sign
+#   solution:
+print("String concatenation is done with the + sign.")
+
+    print('e.g. print("Hello " + "world")')
+#   indentation error
+#   solution
+print("e.g. print('Hello ' + 'world')")    
+
+print(("New lines can be created with a backslash and n.")
+#   too many parentheses on the left-hand side
+#   solution:
+print("New lines can be created with a backslash and n.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
