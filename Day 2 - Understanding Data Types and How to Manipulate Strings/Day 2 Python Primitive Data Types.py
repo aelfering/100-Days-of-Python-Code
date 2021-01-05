4c856069bd5a056a8acaf55d0d0b59025bef29dc
Alex Elfering <anelfering@outlook.com>
2021-01-05 06:37:51 -0600

    day 2 start

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Day 2 Python Primitive Data Types.py
@@ -0,0 +1,17 @@
+#   the len() function calculates the length of a string 
+#   BUT if you use numbers then the script will break
+
+#   data types!!!!
+
+#   String: a string of characters (lol)
+
+"Hello!"
+
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[4])
+
+#   this prints the first character of the string
+#   also known as a subscript
+#   counting in Python starts at 0 bc binaries 
