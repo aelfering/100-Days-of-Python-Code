4a76b5d96514cdd26456ad874ffad80e5c61d5d5
Alex Elfering <anelfering@outlook.com>
2021-01-05 06:42:19 -0600

    finished data types

--- Day 2 - Understanding Data Types and How to Manipulate Strings/Day 2 Python Primitive Data Types.py
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Day 2 Python Primitive Data Types.py
@@ -15,3 +15,83 @@ print("Hello"[4])
 #   this prints the first character of the string
 #   also known as a subscript
 #   counting in Python starts at 0 bc binaries 
+
+
+"123"
+
+#   when integers are surrounded by quotes, then it is a string
+
+print("123" + "456")
+
+#   this concatenates the two strings together
+
+
+
+
+
+#   there is an integer (number with NO decimals)
+
+123
+
+print(123 + 456)
+
+
+#   you can also use underscores to replace commas in large numbers
+
+123_456_789
+
+#   is the same as
+
+123456789
+
+
+
+#   there is a float (numbers with decimals)
+
+3.1456789
+
+
+
+
+
+
+
+
+#   Lastly there is a boolean 
+
+True
+False
+
+#   the first letter is always capitalized and has no quotes around it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
