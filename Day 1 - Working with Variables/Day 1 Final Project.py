f9a72898958087ef8b57ab122f5feaf715992e12
Alex Elfering <anelfering@outlook.com>
2021-01-05 06:31:09 -0600

    fixed questions

--- Day 1 - Working with Variables/Day 1 Final Project.py
+++ Day 1 - Working with Variables/Day 1 Final Project.py
@@ -1,16 +1,15 @@
 # Day 1 Exercise
 
 #1. Create a greeting for the program
-print("Welcome!")
+print("Welcome to the band name generator!")
 
 #2. Ask the user for the city that they grew up in
-city = input("What is the name of the city that you grew up in?")
+city = input("What is the name of the city that you grew up in?\n")
 
 #3. Ask the user for the name of a pet
-pet = input("What is the name of your pet, or a friend's pet?")
+pet = input("What is the name of your pet, or a friend's pet?\n")
 
 #4. Combine the name of their city and pet and show them the name of their band
-print("Your band should be called " + city + " " + pet)
-
+print("Your band could be called " + city + " " + pet)
 
 #5. Make sure the input cursor shows on a new line
