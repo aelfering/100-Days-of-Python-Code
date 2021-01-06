5f77b32ed93349971a7deb4ebd3c31d79626285b
Alex Elfering <anelfering@outlook.com>
2021-01-05 20:36:02 -0600

    Update FINAL PROJECT The Tip Calculator.py

--- Day 2 - Understanding Data Types and How to Manipulate Strings/FINAL PROJECT The Tip Calculator.py
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/FINAL PROJECT The Tip Calculator.py
@@ -6,7 +6,7 @@ total_bill = float(input("What was the total bill?\n"))
 total_tip = float(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
 total_split = int(input("How many people are splitting the bill?\n"))
 
-total_to_pay = (total_bill + (total_bill*(total_tip/100)))/total_split
+total_to_pay = "{:.2f}".format((total_bill + (total_bill*(total_tip/100)))/total_split)
 
 #print(total_to_pay)
 
