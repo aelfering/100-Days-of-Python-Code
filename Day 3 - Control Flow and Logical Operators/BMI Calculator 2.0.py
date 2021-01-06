ad20fd95bce626917f9ef557a17d96eb85453499
Alex Elfering <anelfering@outlook.com>
2021-01-06 05:54:53 -0600

    fixing the formula and adding f-strings

--- Day 3 - Control Flow and Logical Operators/BMI Calculator 2.0.py
+++ Day 3 - Control Flow and Logical Operators/BMI Calculator 2.0.py
@@ -5,15 +5,15 @@ weight = float(input("enter your weight in kg:\n"))
 
 #the formula for BMI is height/weight ** 2
 
-bmi_calc = height/weight ** 2
+bmi_calc = round(weight/height ** 2)
 
 if bmi_calc < 18.5:
-    print("You are underweight")
+    print(f"Your BMI is {bmi_calc}. You are underweight")
 elif bmi_calc >= 18.5 and bmi_calc < 25:
-    print("You have a normal weight")
+    print(f"Your BMI is {bmi_calc}. You have a normal weight")
 elif bmi_calc >= 25 and bmi_calc < 30:
-    print("You are overweight")
+    print(f"Your BMI is {bmi_calc}. You are overweight")
 elif bmi_calc >= 30 and bmi_calc < 35:
-    print("You are obese")
+    print(f"Your BMI is {bmi_calc}. You are obese")
 else:
-    print("You are clinically obese")
+    print(f"Your BMI is {bmi_calc}. You are clinically obese")
