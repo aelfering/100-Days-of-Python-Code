a6c7e18ce24ae4adcd9e86dd3cd708d704f09385
Alex Elfering <anelfering@outlook.com>
2021-01-06 05:20:03 -0600

    kicking off day #3

---
+++ Day 3 - Control Flow and Logical Operators/Day 3 Control Flow with if else and conditional operators.py
@@ -0,0 +1,32 @@
+#   An else if statement is a form of conditional code
+
+#   sample
+#if condition:
+#    do this
+#else:
+#    do this
+    
+#water_level = 50
+#if water_level > 50:
+#        print("Drain the water")
+#else:
+#        print("Keep filling with water")
+
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n"))
+
+#spacing and identation matters A LOT in Python
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you are too short to ride the rollercoaster")
+    
+#   mind your comparison operators
+#   ==      equal to    
+#   >       greater than
+#   <       less than
+#   >=      greater than/equal to
+#   <=      less than/equal to
+#   !=      not equal to    
---
+++ Day 3 - Control Flow and Logical Operators/if else sample.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n"))
+
+#spacing and identation matters A LOT in Python
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Yes")
