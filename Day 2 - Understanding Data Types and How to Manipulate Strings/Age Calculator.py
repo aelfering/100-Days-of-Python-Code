41fddb0dc0ba3a8c5563f80f1647ac6fbfc644f4
Alex Elfering <anelfering@outlook.com>
2021-01-05 20:05:28 -0600

    easier to digest this code

--- Day 2 - Understanding Data Types and How to Manipulate Strings/Age Calculator.py
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Age Calculator.py
@@ -1,16 +1,11 @@
 age = int(input("what is your current age?\n"))
 days = age * 365
-months = days/30
+months = days/31
 weeks = days/7
 
-target_age = 90
-target_days = target_age * 365
-target_months = target_days/30
-target_weeks = target_days/7
+years_left = round(90-age, 1)
+days_left = round(years_left * 365)
+months_left = round(years_left * 12)
+weeks_left = round(years_left * 52)
 
-years_left = round(target_age-age, 1)
-days_left = round(target_days-days, 1)
-months_left = round(target_months-months, 1)
-weeks_left = round(target_weeks-weeks, 1)
-
-print(f"You have roughly {days_left} days, {weeks_left} weeks, {months_left} months, and {years_left} years left until you are {target_age} years old.")
+print(f"You have roughly {days_left} days, {weeks_left} weeks, {months_left} months, and {years_left} years left until you are 90 years old.")
