72fb80c1f68513b8b201835ad706c806217876df
Alex Elfering <anelfering@outlook.com>
2021-01-06 05:55:46 -0600

    simplified

--- Day 3 - Control Flow and Logical Operators/BMI Calculator 2.0.py
+++ Day 3 - Control Flow and Logical Operators/BMI Calculator 2.0.py
@@ -9,11 +9,11 @@ bmi_calc = round(weight/height ** 2)
 
 if bmi_calc < 18.5:
     print(f"Your BMI is {bmi_calc}. You are underweight")
-elif bmi_calc >= 18.5 and bmi_calc < 25:
+elif bmi_calc < 25:
     print(f"Your BMI is {bmi_calc}. You have a normal weight")
-elif bmi_calc >= 25 and bmi_calc < 30:
+elif bmi_calc < 30:
     print(f"Your BMI is {bmi_calc}. You are overweight")
-elif bmi_calc >= 30 and bmi_calc < 35:
+elif bmi_calc < 35:
     print(f"Your BMI is {bmi_calc}. You are obese")
 else:
     print(f"Your BMI is {bmi_calc}. You are clinically obese")
