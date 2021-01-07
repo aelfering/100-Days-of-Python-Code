0d82e9bcc67951770f2f89649666dae41ad6d9a3
Alex Elfering <anelfering@outlook.com>
2021-01-06 20:15:20 -0600

    pizza ordering app

---
+++ Day 3 - Control Flow and Logical Operators/Pizza Ordering Application.py
@@ -0,0 +1,44 @@
+#Pizza Ordering Script
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L?")
+
+add_pepperoni = input("Do you want pepperoni? Y or N?")
+extra_cheese = input("Do you want extra cheese? Y or N?")
+
+final_bill = 0
+
+if size == 'S':
+    final_bill += 15
+    
+    if add_pepperoni == 'Y':
+        final_bill += 2
+    if extra_cheese == 'Y':
+        final_bill += 1
+        
+    print(f"Your final bill comes out to ${final_bill}")    
+    
+elif size == 'M':
+    final_bill += 20
+    
+    if add_pepperoni == 'Y':
+        final_bill += 3
+    if extra_cheese == 'Y':
+        final_bill += 1
+        
+    print(f"Your final bill comes out to ${final_bill}") 
+    
+elif size == 'L':
+    final_bill += 25
+    
+    if add_pepperoni == 'Y':
+        final_bill += 3
+    if extra_cheese == 'Y':
+        final_bill += 1
+        
+    print(f"Your final bill comes out to ${final_bill}") 
+    
+    
+    
+    
+    
